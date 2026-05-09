e8fbc4b9eb33fca490563d1cbc8d459c83aaf882
Александр Садовский <sadovskiyaa@list.ru>
2026-05-09 16:36:21 +0300

    Generic-ViewSet v.2

---
+++ users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone", "city", "is_staff")
+    search_fields = ("email",)
